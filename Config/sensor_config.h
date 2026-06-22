f698c1bff9751f18249649dd6a872f5a48d59837
JithinSreenu <59339409+JithinSreenu@users.noreply.github.com>
2026-06-22 06:39:07 +0530

    Add sensor configuration header file

---
+++ Config/sensor_config.h
@@ -0,0 +1,19 @@
+#ifndef SENSOR_CONFIG_H
+#define SENSOR_CONFIG_H
+
+#define FORCE_MIN                (-200)
+#define FORCE_MAX                (200)
+
+#define KNEE_ANGLE_MIN           (-200)
+#define KNEE_ANGLE_MAX           (200)
+
+#define MOMENT_MIN               (-3000)
+#define MOMENT_MAX               (3000)
+
+#define BATTERY_MIN_MV           (3300)
+#define BATTERY_MAX_MV           (4200)
+
+#define SPOOL_MIN                (0)
+#define SPOOL_MAX                (100)
+
+#endif
