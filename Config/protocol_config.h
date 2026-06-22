662d9afc7997d9ef8a2c2b5e438be5459a9e1dff
JithinSreenu <59339409+JithinSreenu@users.noreply.github.com>
2026-06-22 06:39:55 +0530

    Create protocol_config.h

---
+++ Config/protocol_config.h
@@ -0,0 +1,11 @@
+#ifndef PROTOCOL_CONFIG_H
+#define PROTOCOL_CONFIG_H
+
+#define PACKET_START_BYTE        0xAA
+#define PACKET_STOP_BYTE         0x55
+
+#define PROTOCOL_VERSION         0x01
+
+#define MAX_PACKET_SIZE          64
+
+#endif
