54e91ddd3bcc9f3bbb8137a5b3a504f1d5843590
JithinSreenu <59339409+JithinSreenu@users.noreply.github.com>
2026-06-22 06:36:43 +0530

    Create project_config.h

---
+++ Config/project_config.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_CONFIG_H
+#define PROJECT_CONFIG_H
+
+#define FW_VERSION_MAJOR          1
+#define FW_VERSION_MINOR          0
+
+#define SYSTEM_TICK_MS            1
+
+#define ADC_SAMPLE_RATE_HZ        1000
+
+#define TELEMETRY_PERIOD_MS       50
+
+#define WATCHDOG_PERIOD_MS        100
+
+#endif
